9c02af88212fb289b84d704781ccf4569fb33fcd
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2026-03-13 00:11:08 -0400

    feat(lexer): implement two char chainable token parsing

--- src/Hox/Lexer.hs
+++ src/Hox/Lexer.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE NamedFieldPuns #-}
-
 module Hox.Lexer where
 
 import Combinators.Char (char)
@@ -8,7 +6,8 @@ import Combinators.Repetition (many1)
 import Control.Applicative (Alternative ((<|>)))
 
 data TokenKind
-  = LeftParen
+  = -- Single char tokens
+    LeftParen
   | RightParen
   | LeftBrace
   | RightBrace
@@ -19,6 +18,15 @@ data TokenKind
   | Semicolon
   | Slash
   | Star
+  | -- Single but also two char chainable tokens
+    Bang
+  | BangEq
+  | Eq
+  | DoubleEq
+  | Greater
+  | GreaterEq
+  | Less
+  | LessEq
   deriving (Show, Eq)
 
 data Token = Token
@@ -30,24 +38,59 @@ data Token = Token
 scanTokens :: Parser [Token]
 scanTokens = many1 singleCharToken
 
+twoCharChainableToken :: Parser Token
+twoCharChainableToken = fmap toToken (pair <|> single)
+  where
+    -- TODO: careful with backtracking. prefix (single) is being computed per each parsing possibility
+    toToken str = Token {kind = fromLexeme str, lexeme = str}
+    single = do
+      ch <- char '!' <|> char '=' <|> char '<' <|> char '>'
+      return [ch]
+    pair = do
+      pre <- single
+      eq <- char '='
+      return $ reverse (eq : pre)
+
 singleCharToken :: Parser Token
 singleCharToken =
-  asToken '(' LeftParen
-    <|> asToken ')' RightParen
-    <|> asToken '{' LeftBrace
-    <|> asToken '}' RightBrace
-    <|> asToken ',' Comma
-    <|> asToken '.' Dot
-    <|> asToken '-' Minus
-    <|> asToken '+' Plus
-    <|> asToken ';' Semicolon
-    <|> asToken '/' Slash
-    <|> asToken '*' Star
-  where
-    asToken ch kind = do
-      _ <- char ch
-      return $
-        Token
-          { kind,
-            lexeme = [ch]
-          }
+  charAsToken '('
+    <|> charAsToken ')'
+    <|> charAsToken '{'
+    <|> charAsToken '}'
+    <|> charAsToken ','
+    <|> charAsToken '.'
+    <|> charAsToken '-'
+    <|> charAsToken '+'
+    <|> charAsToken ';'
+    <|> charAsToken '/'
+    <|> charAsToken '*'
+
+charAsToken :: Char -> Parser Token
+charAsToken ch = do
+  str <- fmap (: []) (char ch)
+  return $
+    Token {kind = fromLexeme str, lexeme = str}
+
+fromLexeme :: String -> TokenKind
+fromLexeme str = case str of
+  [] -> error "invalid empty string"
+  "(" -> LeftParen
+  ")" -> RightParen
+  "{" -> LeftBrace
+  "}" -> RightBrace
+  "," -> Comma
+  "." -> Dot
+  "-" -> Minus
+  "+" -> Plus
+  ";" -> Semicolon
+  "/" -> Slash
+  "*" -> Star
+  "!" -> Bang
+  "=" -> Eq
+  "<" -> Less
+  ">" -> Greater
+  "!=" -> BangEq
+  "==" -> DoubleEq
+  "<=" -> LessEq
+  ">=" -> GreaterEq
+  _ -> error "unrecognized lexeme"
--- test/Hox/LexerSpec.hs
+++ test/Hox/LexerSpec.hs
@@ -3,13 +3,13 @@ module Hox.LexerSpec (spec) where
 import Combinators.Parser (ParseState (..), Parser (parse))
 import Combinators.Repetition (many1)
 import Data.Maybe (fromJust, isNothing)
-import Hox.Lexer (Token (..), TokenKind (..), singleCharToken)
+import Hox.Lexer (Token (..), TokenKind (..), singleCharToken, twoCharChainableToken)
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "parses single char token" $ do
-    it "return token for each valid single char lexeme" $ do
+    it "returns token for each valid single char lexeme" $ do
       let input = ParseState "(){},.-+;/*" 1
           result = fst $ fromJust $ parse (many1 singleCharToken) input
           expected =
@@ -27,7 +27,35 @@ spec = do
             ]
       result `shouldBe` expected
 
-    it "fail for invalid single char lexemes" $ do
+    it "fails for invalid single char lexemes" $ do
       let input = ParseState "@" 1
           result = parse singleCharToken input
+      isNothing result `shouldBe` True
+
+  describe "parses single but also two char chainable lexemes" $ do
+    it "returns token for non chained lexemes" $ do
+      let input = ParseState "=!<>" 1
+          result = fst $ fromJust $ parse (many1 twoCharChainableToken) input
+          expected =
+            [ Token Eq "=",
+              Token Bang "!",
+              Token Less "<",
+              Token Greater ">"
+            ]
+      result `shouldBe` expected
+
+    it "returns token for two chained char lexemes" $ do
+      let input = ParseState "==!=<=>=" 1
+          result = fst $ fromJust $ parse (many1 twoCharChainableToken) input
+          expected =
+            [ Token DoubleEq "==",
+              Token BangEq "!=",
+              Token LessEq "<=",
+              Token GreaterEq ">="
+            ]
+      result `shouldBe` expected
+
+    it "fails for invalid chainable char lexemes" $ do
+      let input = ParseState "@" 1
+          result = parse twoCharChainableToken input
       isNothing result `shouldBe` True
