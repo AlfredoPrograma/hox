70c5f512b6b8f8ce69cfe7d5d87715d14785f6bb
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2026-03-13 17:35:27 -0400

    refactor(combinators): remove repeated combinator charPred and just keep satisfy

--- src/Combinators/Char.hs
+++ src/Combinators/Char.hs
@@ -13,15 +13,6 @@ char ch = Parser validate
       | otherwise = Nothing
     validate ParseState {source = []} = Nothing
 
--- Applies predicate to next character of the source and success if True.
-charPred :: (Char -> Bool) -> Parser Char
-charPred f = Parser validate
-  where
-    validate s@ParseState {source = (x : xs)}
-      | f x = Just (x, s {source = xs})
-      | otherwise = Nothing
-    validate ParseState {source = []} = Nothing
-
 -- Applies predicate to the next character of the source and consumes it only if
 -- predicate is True.
 satisfy :: (Char -> Bool) -> Parser Char
